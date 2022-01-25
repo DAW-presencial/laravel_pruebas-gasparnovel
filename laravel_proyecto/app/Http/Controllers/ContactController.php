c7e5b02046e3a3565bdb2b567b35ae068804d93c
gasparnovel <gnovel@cifpfbmoll.eu>
2022-01-25 20:39:58 +0100

    feat: :sparkles: Blade X and Contact form and Tailwindcss

--- laravel_proyecto/app/Http/Controllers/ContactController.php
+++ laravel_proyecto/app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
 
 
 class ContactController extends Controller {
@@ -9,7 +10,10 @@ class ContactController extends Controller {
         return view("contact.form");
     }
 
-    public function send() {
-
+    public function send(Request $request) {
+        $this->validate($request, [
+            "subject" => "required|string|min:5|max:100",
+            
+        ]);
     }
 }
--- laravel_proyecto/resources/views/contact/form.blade.php
+++ laravel_proyecto/resources/views/contact/form.blade.php
@@ -4,4 +4,24 @@
             {{ __('Formulario de contacto') }}
         </h2>
     </x-slot>
+    <div class="py-12">
+        <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-md sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <form method="post" action="{{route ("contact.send")}}">
+                        @csrf
+                        <div class="block mt-4">
+                            <x-form-input name="subject" label="Escribe el motivo de tu mensaje"/>
+                        </div>
+                        <div class="block mt-4">
+                            <x-form-input name="message" label="Escribe tu mensaje aquí"/>
+                        </div>
+                        <div class="flex items-center justify-end mt-4">
+                            <x-form-submit>Enviar mensaje</x-form-submit>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
