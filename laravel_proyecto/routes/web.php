e68662b75eb2a66f90ba1b6aacd7e3f66f7e7c3e
gasparnovel <gnovel@cifpfbmoll.eu>
2022-01-25 13:22:31 +0100

    feat: :sparkles: Breeze and count admin

--- laravel_proyecto/routes/web.php
+++ laravel_proyecto/routes/web.php
@@ -1,12 +1,24 @@
 <?php
 
-use App\Http\Controllers\HolaController;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hola/{name}', HolaController::class);
-Route::get('/hola/{name}/{surname}', [HolaController::class, "surname"]);
-Route::get('/hola/{name}/{surname}/{age}', [HolaController::class, "age"]);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
