6bffa7422465318757ca5aa4606c84832d951828
gasparnovel <gnovel@cifpfbmoll.eu>
2022-01-25 22:47:50 +0100

    feat: :sparkles: Display flash messages and components

--- laravel_proyecto/app/Http/Controllers/ContactController.php
+++ laravel_proyecto/app/Http/Controllers/ContactController.php
@@ -13,15 +13,24 @@ class ContactController extends Controller {
     }
 
     public function send(Request $request) {
+        // se puede meter la validacion dentro del try pero no la mostrara
+        // en su lugar mostrara la excepcion
         $this->validate($request, [
             "subject" => "required|string|min:5|max:100",
             "message" => "required|string|min:20|max:3000",
         ]);
-        Mail::to(User::first())->send(
-            new SendContactForm(
-                $request->input('subject'),
-                $request->input('message'),
-            )
-        );
-    }
+        try {
+            Mail::to(User::first())->send(
+                new SendContactForm(
+                    $request->input('subject'),
+                    $request->input('message'),
+                )
+            );
+            return back()
+                ->with("success", "El mensaje se ha enviado correctamente!");
+        } catch (\Exception $exception) {
+            return back()
+            ->with("error", "Ha fallado el envio del mensaje: " . $exception->getMessage());
+        }
+    } 
 }
