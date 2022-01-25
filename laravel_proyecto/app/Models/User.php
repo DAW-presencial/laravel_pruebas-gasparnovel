79390a4746c0eedbf1a426e2b28c8651fed7cc33
gasparnovel <gnovel@cifpfbmoll.eu>
2022-01-25 13:45:03 +0100

    feat: :sparkles: Email verification using mailtrap

--- laravel_proyecto/app/Models/User.php
+++ laravel_proyecto/app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use App\Models\HasMany;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
